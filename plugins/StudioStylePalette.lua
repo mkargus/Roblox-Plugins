62c2d84924fe90573db65b8e51cc3840f039a30e
michael <14226603+mkargus@users.noreply.github.com>
2023-01-01 09:18:30 -0500

    💄
    Using stylua and selene to clean up this ancient code.

--- plugins/StudioStylePalette.lua
+++ plugins/StudioStylePalette.lua
@@ -1,80 +1,83 @@
 local enabled = false
-local button = plugin:CreateToolbar('Fasty48'):CreateButton('Studio Style Palette','','')
+local button = plugin:CreateToolbar('Fasty48'):CreateButton('Studio Style Palette', '', '')
 
-local ui = plugin:CreateDockWidgetPluginGui('StudioStylePalette', DockWidgetPluginGuiInfo.new(Enum.InitialDockState.Float, false, true, 300, 300, 325, 420))
+local ui = plugin:CreateDockWidgetPluginGui(
+  'StudioStylePalette',
+  DockWidgetPluginGuiInfo.new(Enum.InitialDockState.Float, false, true, 300, 300, 325, 420)
+)
 ui.Title = 'Studio Style Palette'
 ui.Name = 'StudioStylePalette'
 
 local BKG = Instance.new('Frame')
+BKG.Size = UDim2.new(1, 0, 1, 0)
 BKG.Parent = ui
-BKG.Size = UDim2.new(1,0,1,0)
 
 local styleGuideFrame = Instance.new('ScrollingFrame')
 styleGuideFrame.ScrollBarThickness = 0
 styleGuideFrame.ScrollingEnabled = false
-styleGuideFrame.Position = UDim2.new(0,0,0,30)
-styleGuideFrame.Size = UDim2.new(0.25,0,1,-30)
+styleGuideFrame.Position = UDim2.new(0, 0, 0, 30)
+styleGuideFrame.Size = UDim2.new(0.25, 0, 1, -30)
 styleGuideFrame.Name = 'styleGuideFrame'
 styleGuideFrame.Parent = BKG
 
 local ListLayout = Instance.new('UIListLayout')
 ListLayout.Parent = styleGuideFrame
 
-for _,enumItem in pairs(Enum.StudioStyleGuideColor:GetEnumItems()) do
+for _, enumItem in pairs(Enum.StudioStyleGuideColor:GetEnumItems()) do
   local label = Instance.new('TextLabel')
   label.Name = enumItem.Name
   label.Parent = styleGuideFrame
-  label.Size = UDim2.new(1,0,0,30)
+  label.Size = UDim2.new(1, 0, 0, 30)
   label.Font = 'SourceSans'
-  label.Text = ' '..enumItem.Name
+  label.Text = ' ' .. enumItem.Name
   label.TextSize = 16
   label.TextXAlignment = 'Left'
 end
 
-styleGuideFrame.CanvasSize = UDim2.new(0,0,0,ListLayout.AbsoluteContentSize.Y)
+styleGuideFrame.CanvasSize = UDim2.new(0, 0, 0, ListLayout.AbsoluteContentSize.Y)
 
 local ModifierFrame = Instance.new('ScrollingFrame')
 ModifierFrame.ScrollBarThickness = 0
 ModifierFrame.ScrollingEnabled = false
-ModifierFrame.Position = UDim2.new(0.25,0,0,0)
-ModifierFrame.Size = UDim2.new(0.75,0,0,30)
+ModifierFrame.Position = UDim2.new(0.25, 0, 0, 0)
+ModifierFrame.Size = UDim2.new(0.75, 0, 0, 30)
 ModifierFrame.Parent = BKG
 
 local ModifierListLayout = Instance.new('UIListLayout')
 ModifierListLayout.Parent = ModifierFrame
 ModifierListLayout.FillDirection = 'Horizontal'
 
-for _,enumItem in pairs(Enum.StudioStyleGuideModifier:GetEnumItems()) do
+for _, enumItem in pairs(Enum.StudioStyleGuideModifier:GetEnumItems()) do
   local label = Instance.new('TextLabel')
   label.Name = enumItem.Name
-  label.Parent = ModifierFrame
-  label.Size = UDim2.new(0,250,0,30)
+  label.Size = UDim2.new(0, 250, 0, 30)
   label.Font = 'SourceSans'
   label.Text = enumItem.Name
   label.TextSize = 16
+  label.Parent = ModifierFrame
 end
 
-ModifierFrame.CanvasSize = UDim2.new(0,ModifierListLayout.AbsoluteContentSize.X,0,0)
+ModifierFrame.CanvasSize = UDim2.new(0, ModifierListLayout.AbsoluteContentSize.X, 0, 0)
 
 local Main = Instance.new('ScrollingFrame')
 Main.ScrollBarThickness = 8
-Main.Position = UDim2.new(0.25,0,0,30)
-Main.Size = UDim2.new(0.75,0,1,-30)
+Main.Position = UDim2.new(0.25, 0, 0, 30)
+Main.Size = UDim2.new(0.75, 0, 1, -30)
+Main.CanvasSize = UDim2.new(0, ModifierListLayout.AbsoluteContentSize.X, 0, ListLayout.AbsoluteContentSize.Y)
 Main.Parent = BKG
-Main.CanvasSize = UDim2.new(0,ModifierListLayout.AbsoluteContentSize.X, 0, ListLayout.AbsoluteContentSize.Y)
 
 local grid = Instance.new('UIGridLayout')
-grid.CellPadding = UDim2.new(0,0,0,0)
-grid.CellSize = UDim2.new(0,250,0,30)
+grid.CellPadding = UDim2.new(0, 0, 0, 0)
+grid.CellSize = UDim2.new(0, 250, 0, 30)
 grid.Parent = Main
 
-Main:GetPropertyChangedSignal("CanvasPosition"):Connect(function()
-  styleGuideFrame.CanvasPosition = Vector2.new(0,Main.CanvasPosition.Y)
-  ModifierFrame.CanvasPosition = Vector2.new(Main.CanvasPosition.X,0)
+Main:GetPropertyChangedSignal('CanvasPosition'):Connect(function()
+  styleGuideFrame.CanvasPosition = Vector2.new(0, Main.CanvasPosition.Y)
+  ModifierFrame.CanvasPosition = Vector2.new(Main.CanvasPosition.X, 0)
 end)
 
-for _,StudioStyleGuideItem in pairs(Enum.StudioStyleGuideColor:GetEnumItems()) do
-  for _,StudioStyleGuideModifierItem in pairs(Enum.StudioStyleGuideModifier:GetEnumItems()) do
+for _, StudioStyleGuideItem in pairs(Enum.StudioStyleGuideColor:GetEnumItems()) do
+  for _, StudioStyleGuideModifierItem in pairs(Enum.StudioStyleGuideModifier:GetEnumItems()) do
     local Item = Instance.new('Frame')
     Item.BackgroundColor3 = settings().Studio.Theme:GetColor(StudioStyleGuideItem, StudioStyleGuideModifierItem)
     Item.Parent = Main
@@ -84,7 +87,7 @@ for _,StudioStyleGuideItem in pairs(Enum.StudioStyleGuideColor:GetEnumItems()) d
   end
 end
 
-button.Click:connect(function()
+button.Click:Connect(function()
   enabled = not enabled
   ui.Enabled = enabled
 end)
--- plugins/TerrainColorEditor.lua
+++ plugins/TerrainColorEditor.lua
@@ -1,127 +1,135 @@
 --Made By Fasty48
-local Button = plugin:CreateToolbar('Fasty48'):CreateButton('Terrain Color Editor','','rbxassetid://2041394144')
+local Button = plugin:CreateToolbar('Fasty48'):CreateButton('Terrain Color Editor', '', 'rbxassetid://2041394144')
 
-local Color={
-	Black = Color3.fromRGB(33,33,33),
-	Orange = Color3.fromRGB(255,152,0),
-	White = Color3.new(1,1,1)
+local Color = {
+  Black = Color3.fromRGB(33, 33, 33),
+  Orange = Color3.fromRGB(255, 152, 0),
+  White = Color3.new(1, 1, 1),
 }
 
-local MaterialList={
-	{Name='Asphalt',Enum='Asphalt',Default=Color3.fromRGB(115,123,107)},
-	{Name='Basalt',Enum='Basalt',Default=Color3.fromRGB(30,30,37)},
-	{Name='Brick',Enum='Brick',Default=Color3.fromRGB(138,86,62)},
-	{Name='Cobblestone',Enum='Cobblestone',Default=Color3.fromRGB(132,123,90)},
-	{Name='Concrete',Enum='Concrete',Default=Color3.fromRGB(127,102,63)},
-	{Name='Cracked Lava',Enum='CrackedLava',Default=Color3.fromRGB(232,156,74)},
-	{Name='Glacier',Enum='Glacier',Default=Color3.fromRGB(101,176,234)},
-	{Name='Grass',Enum='Grass',Default=Color3.fromRGB(106,127,63)},
-	{Name='Ground',Enum='Ground',Default=Color3.fromRGB(102,92,59)},
-	{Name='Ice',Enum='Ice',Default=Color3.fromRGB(129,194,224)},
-	{Name='Leafy Grass',Enum='LeafyGrass',Default=Color3.fromRGB(115,132,74)},
-	{Name='Limestone',Enum='Limestone',Default=Color3.fromRGB(206,173,148)},
-	{Name='Mud',Enum='Mud',Default=Color3.fromRGB(58,46,36)},
-	{Name='Pavement',Enum='Pavement',Default=Color3.fromRGB(148,148,140)},
-	{Name='Rock',Enum='Rock',Default=Color3.fromRGB(102,108,111),},
-	{Name='Salt',Enum='Salt',Default=Color3.fromRGB(198,189,181)},
-	{Name='Sand',Enum='Sand',Default=Color3.fromRGB(143,126,95)},
-	{Name='Sandstone',Enum='Sandstone',Default=Color3.fromRGB(137,90,71)},
-	{Name='Slate',Enum='Slate',Default=Color3.fromRGB(63,127,107)},
-	{Name='Snow',Enum='Snow',Default=Color3.fromRGB(195,199,218)},
-	{Name='Water',Enum='Water',Default=Color3.fromRGB(12,84,91)},
-	{Name='Wooden Planks',Enum='WoodPlanks',Default=Color3.fromRGB(139,109,79)}
+local MaterialList = {
+  { Name = 'Asphalt', Enum = 'Asphalt', Default = Color3.fromRGB(115, 123, 107) },
+  { Name = 'Basalt', Enum = 'Basalt', Default = Color3.fromRGB(30, 30, 37) },
+  { Name = 'Brick', Enum = 'Brick', Default = Color3.fromRGB(138, 86, 62) },
+  { Name = 'Cobblestone', Enum = 'Cobblestone', Default = Color3.fromRGB(132, 123, 90) },
+  { Name = 'Concrete', Enum = 'Concrete', Default = Color3.fromRGB(127, 102, 63) },
+  { Name = 'Cracked Lava', Enum = 'CrackedLava', Default = Color3.fromRGB(232, 156, 74) },
+  { Name = 'Glacier', Enum = 'Glacier', Default = Color3.fromRGB(101, 176, 234) },
+  { Name = 'Grass', Enum = 'Grass', Default = Color3.fromRGB(106, 127, 63) },
+  { Name = 'Ground', Enum = 'Ground', Default = Color3.fromRGB(102, 92, 59) },
+  { Name = 'Ice', Enum = 'Ice', Default = Color3.fromRGB(129, 194, 224) },
+  { Name = 'Leafy Grass', Enum = 'LeafyGrass', Default = Color3.fromRGB(115, 132, 74) },
+  { Name = 'Limestone', Enum = 'Limestone', Default = Color3.fromRGB(206, 173, 148) },
+  { Name = 'Mud', Enum = 'Mud', Default = Color3.fromRGB(58, 46, 36) },
+  { Name = 'Pavement', Enum = 'Pavement', Default = Color3.fromRGB(148, 148, 140) },
+  { Name = 'Rock', Enum = 'Rock', Default = Color3.fromRGB(102, 108, 111) },
+  { Name = 'Salt', Enum = 'Salt', Default = Color3.fromRGB(198, 189, 181) },
+  { Name = 'Sand', Enum = 'Sand', Default = Color3.fromRGB(143, 126, 95) },
+  { Name = 'Sandstone', Enum = 'Sandstone', Default = Color3.fromRGB(137, 90, 71) },
+  { Name = 'Slate', Enum = 'Slate', Default = Color3.fromRGB(63, 127, 107) },
+  { Name = 'Snow', Enum = 'Snow', Default = Color3.fromRGB(195, 199, 218) },
+  { Name = 'Water', Enum = 'Water', Default = Color3.fromRGB(12, 84, 91) },
+  { Name = 'Wooden Planks', Enum = 'WoodPlanks', Default = Color3.fromRGB(139, 109, 79) },
 }
 
-local Settings={
-	MaterialSelected = 1,
-	PluginEnabled = false,
-	R = 0,
-	G = 0,
-	B = 0
+local Settings = {
+  MaterialSelected = 1,
+  PluginEnabled = false,
+  R = 0,
+  G = 0,
+  B = 0,
 }
 
-local UIButtons={}
+local UIButtons = {}
 
 --------------------
 --UI
 --------------------
-local UI = Instance.new('ScreenGui',game.CoreGui)
+local UI = Instance.new('ScreenGui')
 UI.Enabled = false
+UI.Parent = game.CoreGui
 
 --Color
-local MainFrame = Instance.new('Frame',UI)
+local MainFrame = Instance.new('Frame')
 MainFrame.Active = true
 MainFrame.BackgroundColor3 = Color.Black
-MainFrame.BackgroundTransparency = .5
+MainFrame.BackgroundTransparency = 0.5
 MainFrame.BorderSizePixel = 0
-MainFrame.Position = UDim2.new(0,5,0,5)
-MainFrame.Size = UDim2.new(0,160,0,165)
+MainFrame.Position = UDim2.new(0, 5, 0, 5)
+MainFrame.Size = UDim2.new(0, 160, 0, 165)
+MainFrame.Parent = UI
 
-local header = Instance.new('Frame',MainFrame)
+local header = Instance.new('Frame')
 header.BackgroundColor3 = Color.Orange
 header.BorderSizePixel = 0
-header.Size = UDim2.new(1,0,0,5)
+header.Size = UDim2.new(1, 0, 0, 5)
+header.Parent = MainFrame
 
-local Title = Instance.new('TextLabel',MainFrame)
+local Title = Instance.new('TextLabel')
 Title.BackgroundColor3 = Color.Black
-Title.BackgroundTransparency = .5
+Title.BackgroundTransparency = 0.5
 Title.BorderSizePixel = 0
-Title.Position = UDim2.new(0,5,0,10)
-Title.Size = UDim2.new(1,-10,0,20)
+Title.Position = UDim2.new(0, 5, 0, 10)
+Title.Size = UDim2.new(1, -10, 0, 20)
 Title.Font = Enum.Font.SourceSansBold
 Title.Text = 'Terrain Color Editor'
 Title.TextColor3 = Color.White
 Title.TextSize = 14
+Title.Parent = MainFrame
 
 --Red
-local RedTitle = Instance.new('TextLabel',MainFrame)
+local RedTitle = Instance.new('TextLabel')
 RedTitle.BackgroundColor3 = Color.Black
-RedTitle.BackgroundTransparency = .5
+RedTitle.BackgroundTransparency = 0.5
 RedTitle.BorderSizePixel = 0
-RedTitle.Position = UDim2.new(0,5,0,35)
-RedTitle.Size = UDim2.new(.5,-5,0,20)
+RedTitle.Position = UDim2.new(0, 5, 0, 35)
+RedTitle.Size = UDim2.new(0.5, -5, 0, 20)
 RedTitle.Font = Enum.Font.SourceSansBold
 RedTitle.Text = 'Red'
 RedTitle.TextColor3 = Color.White
 RedTitle.TextSize = 18
 RedTitle.TextXAlignment = Enum.TextXAlignment.Left
+RedTitle.Parent = MainFrame
 
-local RedValue = Instance.new("TextBox",RedTitle)
+local RedValue = Instance.new('TextBox')
 RedValue.BackgroundColor3 = Color.Black
-RedValue.BackgroundTransparency = .5
+RedValue.BackgroundTransparency = 0.5
 RedValue.BorderSizePixel = 0
-RedValue.Position = UDim2.new(1,0,0,0)
-RedValue.Size = UDim2.new(1,0,0,20)
+RedValue.Position = UDim2.new(1, 0, 0, 0)
+RedValue.Size = UDim2.new(1, 0, 0, 20)
 RedValue.Font = Enum.Font.SourceSans
 RedValue.Text = '0'
 RedValue.TextColor3 = Color.White
 RedValue.TextSize = 18
 RedValue.TextXAlignment = Enum.TextXAlignment.Right
+RedValue.Parent = RedTitle
 
-local RedSlider = Instance.new('ScrollingFrame',RedTitle)
+local RedSlider = Instance.new('ScrollingFrame')
 RedSlider.BackgroundColor3 = Color.Black
-RedSlider.BackgroundTransparency = .5
+RedSlider.BackgroundTransparency = 0.5
 RedSlider.BorderSizePixel = 0
-RedSlider.Position = UDim2.new(0,0,0,20)
-RedSlider.Size = UDim2.new(0,150,0,10)
+RedSlider.Position = UDim2.new(0, 0, 0, 20)
+RedSlider.Size = UDim2.new(0, 150, 0, 10)
 RedSlider.BottomImage = 'rbxasset://textures/ui/Scroll/scroll-middle.png'
-RedSlider.CanvasSize = UDim2.new(5,30,0,0)
+RedSlider.CanvasSize = UDim2.new(5, 30, 0, 0)
 RedSlider.MidImage = 'rbxasset://textures/ui/Scroll/scroll-middle.png'
 RedSlider.ScrollBarThickness = 10
 RedSlider.TopImage = 'rbxasset://textures/ui/Scroll/scroll-middle.png'
+RedSlider.Parent = RedTitle
 
 --Green
-local GreenTitle = Instance.new('TextLabel',MainFrame)
+local GreenTitle = Instance.new('TextLabel')
 GreenTitle.BackgroundColor3 = Color.Black
-GreenTitle.BackgroundTransparency = .5
+GreenTitle.BackgroundTransparency = 0.5
 GreenTitle.BorderSizePixel = 0
-GreenTitle.Position = UDim2.new(0,5,0,70)
-GreenTitle.Size = UDim2.new(.5,-5,0,20)
+GreenTitle.Position = UDim2.new(0, 5, 0, 70)
+GreenTitle.Size = UDim2.new(0.5, -5, 0, 20)
 GreenTitle.Font = Enum.Font.SourceSansBold
 GreenTitle.Text = 'Green'
 GreenTitle.TextColor3 = Color.White
 GreenTitle.TextSize = 18
 GreenTitle.TextXAlignment = Enum.TextXAlignment.Left
+GreenTitle.Parent = MainFrame
 
 local GreenValue = RedValue:Clone()
 GreenValue.Parent = GreenTitle
@@ -130,17 +138,18 @@ local GreenSlider = RedSlider:Clone()
 GreenSlider.Parent = GreenTitle
 
 --Blue
-local BlueTitle = Instance.new('TextLabel',MainFrame)
+local BlueTitle = Instance.new('TextLabel')
 BlueTitle.BackgroundColor3 = Color.Black
-BlueTitle.BackgroundTransparency = .5
+BlueTitle.BackgroundTransparency = 0.5
 BlueTitle.BorderSizePixel = 0
-BlueTitle.Position = UDim2.new(0,5,0,105)
-BlueTitle.Size = UDim2.new(.5,-5,0,20)
+BlueTitle.Position = UDim2.new(0, 5, 0, 105)
+BlueTitle.Size = UDim2.new(0.5, -5, 0, 20)
 BlueTitle.Font = Enum.Font.SourceSansBold
 BlueTitle.Text = 'Blue'
 BlueTitle.TextColor3 = Color.White
 BlueTitle.TextSize = 18
 BlueTitle.TextXAlignment = Enum.TextXAlignment.Left
+BlueTitle.Parent = MainFrame
 
 local BlueValue = RedValue:Clone()
 BlueValue.Parent = BlueTitle
@@ -148,187 +157,191 @@ BlueValue.Parent = BlueTitle
 local BlueSlider = RedSlider:Clone()
 BlueSlider.Parent = BlueTitle
 
-local DefaultBtn = Instance.new('TextButton',MainFrame)
+local DefaultBtn = Instance.new('TextButton')
 DefaultBtn.BackgroundColor3 = Color.Black
-DefaultBtn.BackgroundTransparency = .5
+DefaultBtn.BackgroundTransparency = 0.5
 DefaultBtn.BorderSizePixel = 0
-DefaultBtn.Position = UDim2.new(0,5,0,140)
-DefaultBtn.Size = UDim2.new(1,-10,0,20)
+DefaultBtn.Position = UDim2.new(0, 5, 0, 140)
+DefaultBtn.Size = UDim2.new(1, -10, 0, 20)
 DefaultBtn.Font = Enum.Font.SourceSans
 DefaultBtn.Text = 'Reset material to default'
 DefaultBtn.TextColor3 = Color.White
 DefaultBtn.TextSize = 14
+DefaultBtn.Parent = MainFrame
 
 --Material Selector
-local SelectionFrame = Instance.new('ScrollingFrame',MainFrame)
+local SelectionFrame = Instance.new('ScrollingFrame')
 SelectionFrame.BackgroundColor3 = Color.Black
-SelectionFrame.BackgroundTransparency = .5
+SelectionFrame.BackgroundTransparency = 0.5
 SelectionFrame.BorderSizePixel = 0
-SelectionFrame.Position = UDim2.new(1,10,0,0)
-SelectionFrame.Size = UDim2.new(0,150,0,175)
+SelectionFrame.Position = UDim2.new(1, 10, 0, 0)
+SelectionFrame.Size = UDim2.new(0, 150, 0, 175)
 SelectionFrame.BottomImage = 'rbxasset://textures/ui/Scroll/scroll-middle.png'
-SelectionFrame.CanvasSize = UDim2.new(0,0,4,45)
+SelectionFrame.CanvasSize = UDim2.new(0, 0, 4, 45)
 SelectionFrame.MidImage = 'rbxasset://textures/ui/Scroll/scroll-middle.png'
 SelectionFrame.ScrollBarThickness = 5
 SelectionFrame.TopImage = 'rbxasset://textures/ui/Scroll/scroll-middle.png'
+SelectionFrame.Parent = MainFrame
 
-local ListLayout = Instance.new('UIListLayout',SelectionFrame)
-ListLayout.Padding = UDim.new(0,2)
+local ListLayout = Instance.new('UIListLayout')
+ListLayout.Padding = UDim.new(0, 2)
+ListLayout.Parent = SelectionFrame
 
 local Selected = Instance.new('Frame')
 Selected.BackgroundColor3 = Color.Orange
 Selected.BorderSizePixel = 0
-Selected.Size = UDim2.new(0,5,1,0)
+Selected.Size = UDim2.new(0, 5, 1, 0)
 
 --------------------
 --Functions
 --------------------
 function Activate(bool)
-	Settings.PluginEnabled = bool
-	Button:SetActive(bool)
-	UI.Enabled = bool
+  Settings.PluginEnabled = bool
+  Button:SetActive(bool)
+  UI.Enabled = bool
 end
 
 function CheckMaterialColor(Material)
-	local GetColor
+  local GetColor
 
-	if Material == 'Water' then
-		GetColor = workspace.Terrain.WaterColor
-	else
-		GetColor = workspace.Terrain:GetMaterialColor(Enum.Material[Material])
-	end
+  if Material == 'Water' then
+    GetColor = workspace.Terrain.WaterColor
+  else
+    GetColor = workspace.Terrain:GetMaterialColor(Enum.Material[Material])
+  end
 
-	Settings.R = math.floor(GetColor.r*255)
-	RedValue.Text = Settings.R
-	RedSlider.CanvasPosition = Vector2.new(Settings.R,0)
+  Settings.R = math.floor(GetColor.r * 255)
+  RedValue.Text = Settings.R
+  RedSlider.CanvasPosition = Vector2.new(Settings.R, 0)
 
-	Settings.G = math.floor(GetColor.g*255)
-	GreenValue.Text = Settings.G
-	GreenSlider.CanvasPosition = Vector2.new(Settings.G,0)
+  Settings.G = math.floor(GetColor.g * 255)
+  GreenValue.Text = Settings.G
+  GreenSlider.CanvasPosition = Vector2.new(Settings.G, 0)
 
-	Settings.B = math.floor(GetColor.b*255)
-	BlueValue.Text = Settings.B
-	BlueSlider.CanvasPosition = Vector2.new(Settings.B,0)
+  Settings.B = math.floor(GetColor.b * 255)
+  BlueValue.Text = Settings.B
+  BlueSlider.CanvasPosition = Vector2.new(Settings.B, 0)
 end
 
 function CreateBtn(Name)
-	local Btn = Instance.new('TextButton',SelectionFrame)
-	Btn.BackgroundColor3 = Color.Black
-	Btn.BackgroundTransparency = .5
-	Btn.BorderSizePixel = 0
-	Btn.Name = Name
-	Btn.Size = UDim2.new(1,-5,0,30)
-	table.insert(UIButtons,Btn)
-	Btn.LayoutOrder = #UIButtons
-	Btn.Font = Enum.Font.SourceSans
-	Btn.Text = Name
-	Btn.TextColor3 = Color.White
-	Btn.TextSize = 18
-	Btn.MouseButton1Click:connect(function()
-		Selected.Parent = UIButtons[Btn.LayoutOrder]
-		Settings.MaterialSelected = Btn.LayoutOrder
-		CheckMaterialColor(MaterialList[Settings.MaterialSelected].Enum)
-	end)
-	return Btn
+  local Btn = Instance.new('TextButton')
+  Btn.BackgroundColor3 = Color.Black
+  Btn.BackgroundTransparency = 0.5
+  Btn.BorderSizePixel = 0
+  Btn.Name = Name
+  Btn.Size = UDim2.new(1, -5, 0, 30)
+  table.insert(UIButtons, Btn)
+  Btn.LayoutOrder = #UIButtons
+  Btn.Font = Enum.Font.SourceSans
+  Btn.Text = Name
+  Btn.TextColor3 = Color.White
+  Btn.TextSize = 18
+  Btn.MouseButton1Click:Connect(function()
+    Selected.Parent = UIButtons[Btn.LayoutOrder]
+    Settings.MaterialSelected = Btn.LayoutOrder
+    CheckMaterialColor(MaterialList[Settings.MaterialSelected].Enum)
+  end)
+  Btn.Parent = SelectionFrame
+  return Btn
 end
 
 function SetBackToDefault()
-	local Material = Enum.Material[MaterialList[Settings.MaterialSelected].Enum]
-	if Material == Enum.Material.Water then
-		workspace.Terrain.WaterColor = Color3.fromRGB(12,84,91)
-	else
-		workspace.Terrain:SetMaterialColor(Material,MaterialList[Settings.MaterialSelected].Default)
-	end
-	CheckMaterialColor(MaterialList[Settings.MaterialSelected].Enum)
+  local Material = Enum.Material[MaterialList[Settings.MaterialSelected].Enum]
+  if Material == Enum.Material.Water then
+    workspace.Terrain.WaterColor = Color3.fromRGB(12, 84, 91)
+  else
+    workspace.Terrain:SetMaterialColor(Material, MaterialList[Settings.MaterialSelected].Default)
+  end
+  CheckMaterialColor(MaterialList[Settings.MaterialSelected].Enum)
 end
 
 function SetMaterialColor()
-	local Material = Enum.Material[MaterialList[Settings.MaterialSelected].Enum]
-	local Color = Color3.fromRGB(Settings.R,Settings.G,Settings.B)
-	if Material == Enum.Material.Water then
-		workspace.Terrain.WaterColor = Color
-	else
-		workspace.Terrain:SetMaterialColor(Material,Color)
-	end
+  local Material = Enum.Material[MaterialList[Settings.MaterialSelected].Enum]
+  local UserColor = Color3.fromRGB(Settings.R, Settings.G, Settings.B)
+  if Material == Enum.Material.Water then
+    workspace.Terrain.WaterColor = UserColor
+  else
+    workspace.Terrain:SetMaterialColor(Material, UserColor)
+  end
 end
 
 --------------------
 --Material List
 --------------------
 for _, Material in pairs(MaterialList) do
-	CreateBtn(Material.Name)
+  CreateBtn(Material.Name)
 end
 
 --------------------
 --Button events
 --------------------
-DefaultBtn.MouseButton1Click:connect(function()
-	SetBackToDefault()
+DefaultBtn.MouseButton1Click:Connect(function()
+  SetBackToDefault()
 end)
 
 --------------------
 --Textbox events
 --------------------
-RedValue.FocusLost:connect(function()
-	if tonumber(RedValue.Text) then
-		RedSlider.CanvasPosition = Vector2.new(RedValue.Text,0)
-	else
-		RedValue.Text = Settings.R
-	end
+RedValue.FocusLost:Connect(function()
+  if tonumber(RedValue.Text) then
+    RedSlider.CanvasPosition = Vector2.new(RedValue.Text, 0)
+  else
+    RedValue.Text = Settings.R
+  end
 end)
 
-GreenValue.FocusLost:connect(function()
-	if tonumber(GreenValue.Text) then
-		GreenSlider.CanvasPosition = Vector2.new(GreenValue.Text,0)
-	else
-		GreenValue.Text = Settings.G
-	end
+GreenValue.FocusLost:Connect(function()
+  if tonumber(GreenValue.Text) then
+    GreenSlider.CanvasPosition = Vector2.new(GreenValue.Text, 0)
+  else
+    GreenValue.Text = Settings.G
+  end
 end)
 
-BlueValue.FocusLost:connect(function()
-	if tonumber(BlueValue.Text) then
-		BlueSlider.CanvasPosition = Vector2.new(BlueValue.Text,0)
-	else
-		BlueValue.Text = Settings.B
-	end
+BlueValue.FocusLost:Connect(function()
+  if tonumber(BlueValue.Text) then
+    BlueSlider.CanvasPosition = Vector2.new(BlueValue.Text, 0)
+  else
+    BlueValue.Text = Settings.B
+  end
 end)
 
 --------------------
 --Slider events
 --------------------
-RedSlider.Changed:connect(function(property)
-	if property == 'CanvasPosition' then
-		Settings.R = RedSlider.CanvasPosition.X
-		RedValue.Text = RedSlider.CanvasPosition.X
-		SetMaterialColor()
-	end
+RedSlider.Changed:Connect(function(property)
+  if property == 'CanvasPosition' then
+    Settings.R = RedSlider.CanvasPosition.X
+    RedValue.Text = RedSlider.CanvasPosition.X
+    SetMaterialColor()
+  end
 end)
 
-GreenSlider.Changed:connect(function(property)
-	if property == 'CanvasPosition' then
-		Settings.G = GreenSlider.CanvasPosition.X
-		GreenValue.Text = GreenSlider.CanvasPosition.X
-		SetMaterialColor()
-	end
+GreenSlider.Changed:Connect(function(property)
+  if property == 'CanvasPosition' then
+    Settings.G = GreenSlider.CanvasPosition.X
+    GreenValue.Text = GreenSlider.CanvasPosition.X
+    SetMaterialColor()
+  end
 end)
 
-BlueSlider.Changed:connect(function(property)
-	if property == 'CanvasPosition' then
-		Settings.B = BlueSlider.CanvasPosition.X
-		BlueValue.Text = BlueSlider.CanvasPosition.X
-		SetMaterialColor()
-	end
+BlueSlider.Changed:Connect(function(property)
+  if property == 'CanvasPosition' then
+    Settings.B = BlueSlider.CanvasPosition.X
+    BlueValue.Text = BlueSlider.CanvasPosition.X
+    SetMaterialColor()
+  end
 end)
 
 --------------------
 --Plugin Events
 --------------------
-Button.Click:connect(function()
-	Activate(not Settings.PluginEnabled)
+Button.Click:Connect(function()
+  Activate(not Settings.PluginEnabled)
 end)
 
-plugin.Deactivation:connect(function()
-	if Settings.PluginEnabled and UI.Enabled then
-		Activate(false)
-	end
+plugin.Deactivation:Connect(function()
+  if Settings.PluginEnabled and UI.Enabled then
+    Activate(false)
+  end
 end)
